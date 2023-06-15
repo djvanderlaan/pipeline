4ef6ad641f2da423935dac9845f46f97ae56548b
djvanderlaan <jan@ntteloos.nl>
2023-06-15 15:10:15 +0200

    styling

--- scripts/md2html.sh
+++ scripts/md2html.sh
@@ -3,7 +3,7 @@
 IN=$1
 OUT=${IN%.md}.html
 
-pandoc "${IN}" --standalone -t html5 --metadata pagetitle="${IN}" -A scripts/include.html |\
+pandoc "${IN}" --standalone -t html5 --metadata pagetitle="${IN}" -A scripts/include.html -c scripts/pandoc.css |\
   sed -e 's/<pre class="mermaid"><code>/<pre class="mermaid">/g' |\
   sed -e 's/<\/code><\/pre>/<\/pre>/g' >\
   "${OUT}"
